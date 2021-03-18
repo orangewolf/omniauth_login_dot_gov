c1060bc177685a453fffad684911f84f31c02ac6
Rob Kaufman <rob@notch8.com>
2021-03-18 14:13:10 -0700

    Prevent oidc session from causing unexpected exception

--- lib/omniauth/login_dot_gov/callback.rb
+++ lib/omniauth/login_dot_gov/callback.rb
@@ -49,7 +49,7 @@ module OmniAuth
       end
 
       def get_oidc_value_from_session(key)
-        oidc_session = session[:oidc].symbolize_keys
+        oidc_session = session[:oidc]&.symbolize_keys
         return if oidc_session.nil?
         oidc_session[key]
       end
